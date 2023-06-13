84675c639208ec8cb217dce1e70ae7fe3bc6d853
Steven Hutchison <hutchison.music@gmail.com>
2023-06-12 20:35:30 -0500

    change routes to /api base and add /ping endpoint for testing

--- server/index.js
+++ server/index.js
@@ -10,7 +10,10 @@ mongoose
     .connect(uri, {useNewUrlParser: true}).catch(console.log)
     .then(()=>{
         const app = express();
-        app.use('/', WordRouter);
+        app.use('/api', WordRouter);
+        app.get('/ping', (req, res) => {
+            res.send("pong")
+        })
         app.listen(port, () => {
             console.log("Server listening on port", port)
         });
