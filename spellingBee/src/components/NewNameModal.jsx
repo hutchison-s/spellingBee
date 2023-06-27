57e3d10f63ab89e36d9727b00ff07c733267e924
Steven Hutchison <hutchison.music@gmail.com>
2023-06-27 10:53:20 -0500

    validate new name input data through form and input pattern attribute

--- spellingBee/src/components/NewNameModal.jsx
+++ spellingBee/src/components/NewNameModal.jsx
@@ -2,18 +2,24 @@ import PropTypes from 'prop-types'
 import './Modals.css'
 
 export default function NewNameModal({onClick}) {
+
+    function onSubmit(e) {
+        e.preventDefault();
+        if (e.target.gamerNameInput.value !== '') {
+            onClick(e.target.gamerNameInput.value);
+        }
+        e.target.reset();
+    }
+
     return (
         <dialog id="newName">
             <img src="/bee-honey-icon.svg" alt="bee logo" />
             <h3 className="modalSectionHead">Choose a Display Name</h3>
-            <input type="text" minLength={2} maxLength={14} />
-            <button
-            onClick={(e) =>
-                onClick(e.target.parentNode.querySelector("input").value)
-            }
-            >
-            Continue
-            </button>
+            <form onSubmit={onSubmit}>
+                <label htmlFor="gamerNameInput"><small>Letters and numbers only, 2-14 characters</small></label>
+                <input type="text" pattern='^[A-Za-z\d]{2,14}$' name='gamerNameInput' id='gamerNameInput'/>
+                <button type='submit'>Continue</button>
+            </form>
         </dialog>
     )
 }
